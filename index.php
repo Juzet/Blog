4c8baf0934fc1d2dc4074d7c29d214dec8b4b6e5
Juzet Arana <arana.juzet@gmail.com>
2014-10-28 11:11:21 -0700

    Revert "."
    This reverts commit 42c9dec087e8cdb87460b474d6e09a05d9ede3b6.

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// helps the webpage run locally
-	require_once (__DIR__ . "/controller/create-db.php");
-?>
--- model/database.php
+++
@@ -1,6 +0,0 @@
-<!-- variables that are used to help the code run and make the coding easier  -->
-<?php
-	$host = "localhost";
-	$username = "root";
-	$password = "root";
-	$database = "blog_db";
