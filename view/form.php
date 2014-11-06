71c82f53add265e6ad39d59d38516c7c343792df
Juzet Arana <arana.juzet@gmail.com>
2014-11-06 11:09:55 -0800

    media queries

--- view/form.php
+++ view/form.php
@@ -1,12 +1,21 @@
+<div id="one">
 <h1>Create a Blog Post</h1>
-
+</div>
 <form>
-	<div>
-		<label for="title"></label>
+	<div id="title">
+		<!-- labels the boxes -->
+		<label for="title">Title:</label>
+		<!-- allows you to put a single line -->
 		<input type="text" name="title"/>
 	</div>
-	<div>
+	<div id="post">
+		<!-- lables the post box -->
 		<label for="post">Post: </label>
+		<!-- allows you input multiple lines -->
 		<textarea name="post"></textarea>
+	</div>
+		<!-- allows me to make a submit button -->
+	<div>
+		<button type="submit">Submit</button>
 	</div>
 </form>
