abac6a72a68b57d7068b87ee25203cdb70fb948a
Juzet Arana <arana.juzet@gmail.com>
2014-11-17 09:03:23 -0800

    finished up to video 31 no comments though.

--- model/database.php
+++ model/database.php
@@ -8,11 +8,33 @@ class Database {
 	private $password;
 	private $database;
 
+	// This is making the class suitable for any initialization that the object may call before it is used
 	public function__construct($host, $username, $password, $database) {
 		$this->host = $host;
 		$this->username= $username;
 		$this->password= $password;
 		$this->database= $database;
+	}
+
+	public function openConnection() {
+		$this->connection - new mysqli($this->host, $this->username, $this->password, $this->database );
+
+		if (this->$connection->connect_error) {
+			die("<p>Error:" . $this->connection->connect_error . "</p>");
+		}
+	}
+	public function closeConnection(){
+		if(isset ($this->connection)){
+			$this_>connection->close();
+		}
+	}
+	public function query($string) {
+		$this->openConnection();
+
+		$query = $this->connection->query($string);
+
+		$this->closeConnection();
+		return $query;
 	}
 
 }
