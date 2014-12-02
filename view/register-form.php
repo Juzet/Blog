7ac79c58efb6a02de4432314cc4ba41dfee2ef35
Juzet Arana <arana.juzet@gmail.com>
2014-12-02 11:04:46 -0800

    created a register form first step to adding users to the blog

---
+++ view/register-form.php
@@ -0,0 +1,22 @@
+<form>
+	<div>
+		<label for="email"> Email: </label>
+		<input type="text" name="email"/>
+	</div>
+
+	<div>
+			<label for="username"> Username: </label>
+			<input type"text" name="username">
+	</div>
+
+	<div>
+		<label for="password"> Password: </label>
+		<input type"password" name="password">
+	</div>
+
+	<div>
+		<button type="submit"> Submit </button>
+	</div>
+</form>
+<!-- we are creating input for the user to put a username, passowrd, and email -->
+<!-- for the input for password we put password because we don't want the words to show on the screen -->
