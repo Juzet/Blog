637a27a5e5030a55fcb396dbf60f7b94eabbc2cf
Juzet Arana <arana.juzet@gmail.com>
2014-12-05 18:27:28 -0800

    successfully finished video 42 adding the comments however in video 43 will change password entity

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,3 +1,14 @@
 <?php
+// we are getting access to the data in the folder model file config
+	require_once(__DIR__ . "/../model/config.php");
 
-echo "Successfully created Users";
+// sanitizing the email so that it will delete unknown charaters in the email 
+	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+// sanitizing by string so any invalid characters will automatically be deleted using a string
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+// sanitizing the passowrd so any invalid characters will automatically be deleted using a string 
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+
+
+	echo $email . " - " . $username . " - " . $password;
