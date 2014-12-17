814223a70e05898330871c5b227d1c8bdfd5e8b8
Juzet Arana <arana.juzet@gmail.com>
2014-12-16 21:52:50 -0800

    commented and indented

--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,8 @@
-<!-- closing the header tags -->
+<!-- closing the footer tags -->
 <footer>
 		&copy Juzet Arana
 </footer>
+<!-- the script tag are used to help sylize bootstrap add a jquery and add the readmore -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="js/readmore.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
