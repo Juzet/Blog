b3975376547772a7e79701b3ad4b289123e35cd9
Juzet Arana <arana.juzet@gmail.com>
2014-12-16 21:41:56 -0800

    commented and indented properly

--- controller/login-verify.php
+++ controller/login-verify.php
@@ -1,12 +1,14 @@
 <?php
-
+// requiring the information from the config file
 	require_once(__DIR__ . "/../model/config.php");
-
+// this is verifying the the the login 
 	function authenticateUser() {
 		if(!isset($_SESSION["authenticated"])) {
+// / the isset is authenticating the user and setting the session to it
 			return false;
 		}
 		else {
+// if the session variable is false then the session will return false
 			if($_SESSION["authenticated"] != true) {
 				return false;
 			}
--- controller/logout-user.php
+++ controller/logout-user.php
@@ -1,7 +1,9 @@
 <?php
+// requiring the config file 
 	require_once(__DIR__ . "/../model/config.php");
-
+// this is unsetting the authentication
 	unset($_SESSION["authenticated"]);
-
+// this is destroying the session variable 
 	session_destroy();
+// making the header path go to the index
 	header("Location: " . $path . "index.php");
